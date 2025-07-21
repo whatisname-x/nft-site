53d6d785f691ff874efa84011a258c8d3292b7e0
venoosoo <sergiy1337133764@gmail.com>
2025-07-21 20:54:45 +0300

    btn

--- my-react-app/src/Greeting.jsx
+++ my-react-app/src/Greeting.jsx
@@ -20,14 +20,14 @@ export default function Greeting() {
 
 
   return (
-    <div className="h-screen font-sans flex flex-col items-center justify-center text-center p-4 bg-gray-800 text-white">
+    <div className="h-screen relative font-sans flex flex-col items-center justify-center text-center p-4 bg-gray-800 text-white">
       <h1 className="text-3xl font-bold mb-6">Добро пожаловать в {bot || 'наш магазин'}</h1>
       <h3 className="text-gray-400 mb-8">
         Здесь начинается мир продажи и обмена подарками
       </h3>
       <button
         onClick={handleClick}
-        className="bg-blue-600 px-6 py-3 rounded-lg text-white hover:bg-blue-700 transition"
+        className="bg-blue-600 mt-auto absolute bottom-6 px-6 py-3 rounded-lg text-white hover:bg-blue-700 transition"
       >
         Продолжить
       </button>
