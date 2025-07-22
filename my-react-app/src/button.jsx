21e9b92d42c839af1184c471d9cb19544b2f96e3
whatisname-x <X0xpl@tutamail.com>
2025-07-23 02:02:39 +0300

    Update button.jsx

--- my-react-app/src/button.jsx
+++ my-react-app/src/button.jsx
@@ -14,11 +14,13 @@ export default function TelegramButton({ text = 'Continue', redirectTo = '/' }) 
     <button
       onClick={handleClick}
       className="
-        fixed bottom-6 font-sans left-1/2 -translate-x-1/2 z-50
-        bg-[#2AABEE] bg-opacity-95 backdrop-blur-sm
-        w-[90%] text-white text-base font-medium
-        px-6 py-3 rounded-xl shadow-lg
+        fixed bottom-6 left-1/2 -translate-x-1/2 z-50
+        bg-[#2da7e0] bg-opacity-95 backdrop-blur-sm
+        w-[88%] text-white text-sm font-medium
+        px-6 py-2.5 rounded-lg shadow-lg
+        hover:bg-[#199ad9]
         transition active:scale-95
+        font-sans
       "
     >
       {text}
